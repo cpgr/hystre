b5f772be501ef0ceb25dfd1db2259d10080994a8
Chris Green <chris.green@csiro.au>
2020-08-28 20:41:14 +1000

    Initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+hystre
+=====
+
+"Fork hystre" to create a new MOOSE-based application.
+
+For more information see: [http://mooseframework.org/create-an-app/](http://mooseframework.org/create-an-app/)
---
+++ src/base/hystreApp.C
@@ -0,0 +1,53 @@
+#include "hystreApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+hystreApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  // Do not use legacy DirichletBC, that is, set DirichletBC default for preset = true
+  params.set<bool>("use_legacy_dirichlet_bc") = false;
+
+  return params;
+}
+
+hystreApp::hystreApp(InputParameters parameters) : MooseApp(parameters)
+{
+  hystreApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+hystreApp::~hystreApp() {}
+
+void
+hystreApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ModulesApp::registerAll(f, af, s);
+  Registry::registerObjectsTo(f, {"hystreApp"});
+  Registry::registerActionsTo(af, {"hystreApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+hystreApp::registerApps()
+{
+  registerApp(hystreApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+hystreApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  hystreApp::registerAll(f, af, s);
+}
+extern "C" void
+hystreApp__registerApps()
+{
+  hystreApp::registerApps();
+}
